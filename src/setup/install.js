103993f529ef9860124ec1a68ecafe9154a0d05d
DEMONITIZED BOI <42183865+demonitize@users.noreply.github.com>
2022-01-06 09:50:52 -0800

    Update install.js

--- src/setup/install.js
+++ src/setup/install.js
@@ -1,1 +1,22 @@
+/* 
+  Install Script. Setup the config files if they don't already exist, and test for required packages
+*/
+const OS = require("os");
 
+switch(OS.platform()) {
+  default:
+    console.warn("You are running on an unknown OS. Package is not guaranteed to function correctly");
+    break;
+  case "aix":
+  case "darwin":
+  case "freebsd":
+  case "linux":
+  case "openbsd":
+  case "sunos":
+    console.log("You are running on a supported version of Linux!");
+    break;
+  case "win32":
+  case "win64":
+    console.log("You are running Windows. Expect the normal Windows bugs"); /* just gonna roast Windows here */
+    break;
+}
